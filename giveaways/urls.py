b39754e888ba2ebe47c3ac3bb04f77b6f88e1769
username <user@email.com>
2023-10-27 20:00:58 +0300

    fixed current giveaway not found

--- giveaways/urls.py
+++ giveaways/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
         "detail/<int:pk>/", views.giveaway_detail, name="giveaways-giveaway-detail"
     ),  # /api/giveaway/detail/1/
     path(
+        "detail/", views.giveaway_detail, name="giveaways-giveaway-detail"
+    ),  # /api/giveaway/detail/
+    path(
         "participate/", views.participate, name="giveaways-participate"
     ),  # /api/giveaway/participate/
 ]
