558709d088b280fec93ee45b127e810bb4b0e294
username <user@email.com>
2023-10-27 22:03:02 +0300

    changes to orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from orders.models import Orders
-
+from orders.models import Orders, Wallet
 
 admin.site.register(Orders)
+admin.site.register(Wallet)
--- orders/models.py
+++ orders/models.py
@@ -48,27 +48,27 @@ class Wallet(models.Model):
         pass
 
 
-@receiver(post_save, sender=Orders)
-def send_order_after_create(sender, instance, created, **kwargs):
-    with transaction.atomic():
-        if instance.is_test_instance:
-            return
-        if created:
-            if Orders.objects.filter(is_in_queue=True).count() < 10:
-                instance.is_in_queue = True
-                instance.save()
-
-                wallet = Wallet.objects.get(id=0)
-                queue_price = Orders.objects.filter(is_in_queue=True).aggregate(Sum('item__ph_price'))['item__ph_price__sum']
-                money_left = wallet.total_balance
-                difference = queue_price - money_left
-                if difference >= 0:
-                    money_to_send = wallet.default_payment
-                    if difference > money_to_send:
-                        money_to_send += difference - money_to_send
-
-                    wallet.total_balance += money_to_send
-                    wallet.save()
-                    # TODO: Send money to the seller and a notification in the bot
-
-                misc.wittebane.send_order(instance)
+# @receiver(post_save, sender=Orders)
+# def send_order_after_create(sender, instance, created, **kwargs):
+#     with transaction.atomic():
+#         if instance.is_test_instance:
+#             return
+#         if created:
+#             if Orders.objects.filter(is_in_queue=True).count() < 10:
+#                 instance.is_in_queue = True
+#                 instance.save()
+#
+#                 wallet = Wallet.objects.get(id=0)
+#                 queue_price = Orders.objects.filter(is_in_queue=True).aggregate(Sum('item__ph_price'))['item__ph_price__sum']
+#                 money_left = wallet.total_balance
+#                 difference = queue_price - money_left
+#                 if difference >= 0:
+#                     money_to_send = wallet.default_payment
+#                     if difference > money_to_send:
+#                         money_to_send += difference - money_to_send
+#
+#                     wallet.total_balance += money_to_send
+#                     wallet.save()
+#                     # TODO: Send money to the seller and a notification in the bot
+#
+#                 misc.wittebane.send_order(instance)
