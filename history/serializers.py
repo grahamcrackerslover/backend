6fecc5396905ffa294177c27cb109707473ce0be
username <user@email.com>
2023-10-27 19:55:43 +0300

    fixed inventory view

--- history/serializers.py
+++ history/serializers.py
@@ -1,9 +1,13 @@
 # serializers.py
 from rest_framework import serializers
+
+from cases.serializers import ItemSerializer
 from .models import HistoryItem, HistoryCase
 
 
 class HistoryItemSerializer(serializers.ModelSerializer):
+    item = ItemSerializer()
+
     class Meta:
         model = HistoryItem
         fields = "__all__"
