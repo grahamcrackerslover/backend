5ced43055aa1106ffe25205876d072326abc1d97
grahamcrackerslover <>
2023-10-29 02:05:10 +0300

    changed review serializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+
+from custom_user.serializers import BasicUserSerializer
 from .models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = BasicUserSerializer()
+
     class Meta:
         model = Review
         fields = ("id", "is_positive", "text", "author", "created_at")
