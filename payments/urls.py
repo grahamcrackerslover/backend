50affdb8875c76dfd5d85435bf3856b6db656deb
grahamcookieslover <notyouraverageboi@protom.me>
2023-12-06 18:58:23 +0200

    minor changes to urls

--- payments/urls.py
+++ payments/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('verify/<str:code>/', views.verify_promo, name='payments-verify-promo'), 
     path('buy/', views.shop_buy, name='payments-shop-buy'),
     path('uid_details/', views.uid_details, name='payments-uid-details'),
-    path('payments/<int:pk>/', views.payment_detail, name='payment-detail'),
+    path('details/<int:pk>/', views.payment_detail, name='payments-payment-detail'),
 ]
